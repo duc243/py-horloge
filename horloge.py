e1a3ddb29bd9ffbda88df6f2c9066619b447c38b
Exousia KASEKA <kasekaexousia@gmail.com>
2024-03-21 09:54:56 +0100

    Lift the paintbrush and move it forward for a distance

---
+++ horloge.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+import turtle
+from datetime import *
+
+# Lift the paintbrush and move it forward for a distance
+def Skip(step):
+    turtle.penup()
+    turtle.forward(step)
+    turtle.pendown()
+
