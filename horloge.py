78a46a2cf8e1cbee921d8e5be489dd0f59b4f3cc
Exousia KASEKA <kasekaexousia@gmail.com>
2024-03-21 10:02:56 +0100

    feat: Add custom hand shape using Turtle graphics

--- horloge.py
+++ horloge.py
@@ -9,3 +9,15 @@ def Skip(step):
     turtle.forward(step)
     turtle.pendown()
 
+def mkHand(name, length):
+    # Register the Turtle shape and create the hand Turtle
+    turtle.reset()
+    Skip(-length * 0.1)
+    # Start recording the vertices of the polygon. The current turtle position is the first vertex of the polygon.
+    turtle.begin_poly()
+    turtle.forward(length * 1.1)
+    # Stop recording the vertices of the polygon. The current turtle position is the last vertex of the polygon. Will be connected to the first vertex.
+    turtle.end_poly()
+    # Return the last recorded polygon.
+    handForm = turtle.get_poly()
+    turtle.register_shape(name, handForm)
